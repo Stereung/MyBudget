7eeac3c6209a1980636b9f7f799b51c3b59efeca
Stereung <private.stereung@gmail.com>
2019-12-10 22:38:35 +0700

    Create main class; *Hello worldadd Common.cs console application

---
+++ Common.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyBudget
+{
+    class Common
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world!");
+        }
+    }
+}
